f87e158f79c28c26d3abcee2c9f8524be1626a11
ChampionBuffalo1 <vidish7nov@gmail.com>
2023-07-24 11:54:30 +0530

    Connecting to mongoDb

---
+++ src/lib/connect.ts
@@ -0,0 +1,51 @@
+import mongoose from 'mongoose';
+import Logger from './Logger';
+
+let retryCount = 0,
+  mongoConnected = false;
+const maxRetries = 5,
+  MINUTE = 1000 * 60,
+  defaultDelay = 5000; // 5s
+
+const createMongoConnection = (): Promise<void> =>
+  new Promise((resolve, reject) => {
+    if (!mongoConnected) {
+      const dbOptions: mongoose.ConnectOptions = {
+        family: 4,
+        maxPoolSize: 100,
+        retryWrites: true,
+        connectTimeoutMS: 30000
+      };
+      mongoose
+        .set('strictQuery', true)
+        .connect(process.env.MONGO_URL, dbOptions)
+        .then(() => {
+          mongoConnected = true;
+          resolve();
+        })
+        .catch(err => {
+          retryCount++;
+          Logger.error((err as Error).toString());
+          Logger.debug(`MongoDB connection failure count: ${retryCount}`);
+          if (retryCount > maxRetries) reject('Maximum mongo re-connect tries exceeded.');
+          setTimeout(createMongoConnection, defaultDelay);
+        });
+
+      mongoose.connection.on('connected', () => Logger.info('MongoDB connected successfully.'));
+      mongoose.connection.on('open', () => Logger.debug('onOpen has successfully executed on all model'));
+      mongoose.connection.on('error', err => {
+        retryCount = 0;
+        mongoConnected = false;
+
+        const retryTime = MINUTE * 2;
+
+        setTimeout(createMongoConnection, retryTime);
+        Logger.error(`MongoDB connection lost.\n${err}`);
+        Logger.info(`Re-connecting to MongoDB in ${retryTime / MINUTE} minutes.`);
+      });
+    } else {
+      resolve();
+    }
+  });
+
+export { createMongoConnection };
