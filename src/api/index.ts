e9609b10f826260b3fa7000d9c2a0199ab60891f
ChampionBuffalo1 <vidish7nov@gmail.com>
2023-07-24 11:36:24 +0530

    Changed the test message

--- src/api/index.ts
+++ src/api/index.ts
@@ -3,7 +3,7 @@ import { Router } from 'express';
 const apiRouter = Router();
 
 apiRouter.get('/', (_, res) => {
-  res.status(200).json({ message: 'Hello World!' });
+  res.status(200).json({ message: 'Hello from API Router' });
 });
 
 export default apiRouter;
