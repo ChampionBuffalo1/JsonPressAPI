c7c4d472f2221e4e88e45030c5b5bac908d13bb4
ChampionBuffalo1 <vidish7nov@gmail.com>
2023-07-24 11:36:34 +0530

    Added a / get

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,16 @@
-import express from 'express';
 import cors from 'cors';
+import express from 'express';
+import apiRouter from './api';
 import Logger from './lib/Logger';
-import api from './api';
 
 (async () => {
   const app = express();
   app.use(cors(), express.json(), express.urlencoded({ extended: true }));
-  app.use('/api', api);
+  app.use('/api', apiRouter);
+
+  app.get('/', (_, res) => {
+    res.send('Hello World!');
+  });
 
   const PORT = process.env.API_PORT || 3000;
   app.listen(PORT, () => {
