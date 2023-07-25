107f8c621fdfa14500f549a901f45aed5045dc9e
ChampionBuffalo1 <vidish7nov@gmail.com>
2023-07-25 22:54:02 +0530

    Invalidating jwt tokens

--- src/lib/passport.ts
+++ src/lib/passport.ts
@@ -11,7 +11,12 @@ const opts: StrategyOptions = {
 passport.use(
   new JwtStrategy(opts, async (jwt_payload, done) => {
     try {
-      const user = await User.findOne({ id: jwt_payload.id });
+      const user = await User.findOne({ id: jwt_payload.id }, '_id updatedAt', { lean: true });
+      // User object has been changed so the token is no longer valid
+      // To tackle the problem where the user has been deleted (or has changed password)
+      // and can still use the token to perform actions
+      // FIXME: This means updating socials or image for the user also invalidates the token
+      if (user?.updatedAt && Date.parse(user.updatedAt.toString()) / 1000 > jwt_payload.iat) return done(null, false);
       if (user) return done(null, user);
     } catch (err) {
       return done(err, false);
