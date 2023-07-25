12ea822b707e612264bb487d5eb89001da6d702d
ChampionBuffalo1 <vidish7nov@gmail.com>
2023-07-25 12:43:39 +0530

    Creating MongoConnection and init passport

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,17 @@ import cors from 'cors';
 import express from 'express';
 import apiRouter from './api';
 import Logger from './lib/Logger';
+import { config } from 'dotenv-safe';
+import passport from './lib/passport';
+import { createMongoConnection } from './lib/connect';
 
 (async () => {
+  config();
   const app = express();
   app.use(cors(), express.json(), express.urlencoded({ extended: true }));
+  await createMongoConnection();
+
+  app.use(passport.initialize());
   app.use('/api', apiRouter);
 
   app.get('/', (_, res) => {
