a46b578f10187d345d0f8e73cc8b8bb6d5f08275
ChampionBuffalo1 <vidish7nov@gmail.com>
2023-07-25 20:57:32 +0530

    Added bcryptRound constants

--- src/Constants.ts
+++ src/Constants.ts
@@ -2,3 +2,4 @@ export const isProd: boolean = process.env.NODE_ENV === 'production';
 // eslint-disable-next-line @typescript-eslint/no-empty-function
 export const noop = () => {};
 export const JwtSecret = process.env.JWT_SECRET || 'secret';
+export const bcryptRounds = 8;
