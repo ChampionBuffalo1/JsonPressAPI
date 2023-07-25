f1bb8ac0aa69ec3b824fd7411b0231e7e56d0f6d
ChampionBuffalo1 <vidish7nov@gmail.com>
2023-07-25 12:43:22 +0530

    Added JwtSecret

--- src/Constants.ts
+++ src/Constants.ts
@@ -1,3 +1,4 @@
 export const isProd: boolean = process.env.NODE_ENV === 'production';
 // eslint-disable-next-line @typescript-eslint/no-empty-function
 export const noop = () => {};
+export const JwtSecret = process.env.JWT_SECRET || 'secret';
