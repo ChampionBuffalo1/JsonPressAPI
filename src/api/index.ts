af6ac3c75a89c339c64546f4ebe5ee277f956826
ChampionBuffalo1 <vidish7nov@gmail.com>
2023-07-25 12:43:52 +0530

    Added user Router

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
+import userRouter from './route/userRoute';
 
 const apiRouter = Router();
-
 apiRouter.get('/', (_, res) => {
   res.status(200).json({ message: 'Hello from API Router' });
 });
+
+apiRouter.use('/user', userRouter);
 
 export default apiRouter;
