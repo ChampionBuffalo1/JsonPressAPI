835a0ed04463cfffe11b003db26ba628033451b1
ChampionBuffalo1 <vidish7nov@gmail.com>
2023-07-25 12:56:47 +0530

    Added Blog model

---
+++ src/models/Blog.ts
@@ -0,0 +1,41 @@
+import User from './User';
+import { Schema, model } from 'mongoose';
+import { BlogType } from '../typings/model';
+
+const BlogSchema = new Schema<BlogType>({
+  name: {
+    type: String,
+    required: [true, 'name is required']
+  },
+  content: {
+    type: String,
+    required: [true, 'content is required']
+  },
+  coverImage: {
+    type: String,
+    required: [true, 'coverImage is required']
+  },
+  slug: {
+    type: String,
+    required: [true, 'blog slug is required']
+  },
+  author: {
+    type: User,
+    required: [true, 'blog author is required']
+  },
+  isPublished: {
+    type: Boolean,
+    default: false
+  },
+  views: {
+    type: Number,
+    default: 0
+  },
+  category: String,
+  lastEditedAt: {
+    type: Date,
+    required: true
+  }
+});
+
+export default model('Blogs', BlogSchema);
