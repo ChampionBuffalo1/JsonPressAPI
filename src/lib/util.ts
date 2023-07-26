fe0f61074bf317be3095996193a3c4b1ce10b53b
ChampionBuffalo1 <vidish7nov@gmail.com>
2023-07-26 13:03:08 +0530

    moved getZodError

--- src/lib/util.ts
+++ src/lib/util.ts
@@ -1,4 +1,5 @@
 import jwt from 'jsonwebtoken';
+import type { ZodIssue } from 'zod';
 import { JwtSecret } from '../Constants';
 
 function generateJwtToken(id: string, expiresIn?: string): string {
@@ -6,4 +7,14 @@ function generateJwtToken(id: string, expiresIn?: string): string {
   return jwt.sign(payload, JwtSecret, expiresIn ? { expiresIn } : {});
 }
 
-export { generateJwtToken };
+function getZodError(issues: ZodIssue[]) {
+  const errorMessages: Record<string, string> = {};
+  for (const issue of issues) {
+    if (issue.path) {
+      errorMessages[issue.path.join('.')] = issue.message;
+    }
+  }
+  return errorMessages;
+}
+
+export { generateJwtToken, getZodError };
