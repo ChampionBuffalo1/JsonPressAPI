3fc10494d37181936059cc1471f834f7b05a27bb
ChampionBuffalo1 <vidish7nov@gmail.com>
2023-07-27 14:16:29 +0530

    Added updateSchema

--- src/validators/blogValidator.ts
+++ src/validators/blogValidator.ts
@@ -1,10 +1,21 @@
 import { z } from 'zod';
 
 const createSchema = z.object({
-  title: z.string().min(1).max(255),
-  content: z.array(z.record(z.string(), z.unknown())),
   slug: z.string().min(1).max(64),
-  category: z.string().min(1).max(64)
+  title: z.string().min(1).max(255),
+  category: z.string().min(1).max(64),
+  coverImage: z.string().optional(),
+  content: z.array(z.record(z.string(), z.unknown()))
 });
 
-export { createSchema };
+const updateSchema = z.object({
+  coverImage: z.string().optional(),
+  slug: z.string().min(1).max(64).optional(),
+  title: z.string().min(1).max(255).optional(),
+  category: z.string().min(1).max(64).optional(),
+  content: z.array(z.record(z.string(), z.unknown())).optional()
+});
+
+type updateSchema = z.infer<typeof updateSchema>;
+
+export { createSchema, updateSchema };
