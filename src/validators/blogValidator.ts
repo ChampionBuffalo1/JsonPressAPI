2f9a74390ce2fdff3e152f8dc17361613a4270e1
ChampionBuffalo1 <vidish7nov@gmail.com>
2023-07-28 20:42:07 +0530

    Added validation error

--- src/validators/blogValidator.ts
+++ src/validators/blogValidator.ts
@@ -1,18 +1,42 @@
 import { z } from 'zod';
+import { getRangeError } from '../lib';
 
 const createSchema = z.object({
-  slug: z.string().min(1).max(64),
-  title: z.string().min(1).max(255),
-  category: z.string().min(1).max(64),
+  slug: z
+    .string()
+    .min(1)
+    .max(64, getRangeError('slug', 1, 64)),
+  title: z
+    .string()
+    .min(1)
+    .max(255, getRangeError('title', 1, 255)),
+  category: z
+    .string()
+    .min(1)
+    .max(64, getRangeError('slug', 1, 64)),
   coverImage: z.string().optional(),
+  description: z.string().optional(),
   content: z.array(z.record(z.string(), z.unknown()))
 });
 
 const updateSchema = z.object({
   coverImage: z.string().optional(),
-  slug: z.string().min(1).max(64).optional(),
-  title: z.string().min(1).max(255).optional(),
-  category: z.string().min(1).max(64).optional(),
+  description: z.string().optional(),
+  slug: z
+    .string()
+    .min(1)
+    .max(64, getRangeError('slug', 1, 64))
+    .optional(),
+  title: z
+    .string()
+    .min(1)
+    .max(255, getRangeError('title', 1, 255))
+    .optional(),
+  category: z
+    .string()
+    .min(1)
+    .max(64, getRangeError('category', 1, 64))
+    .optional(),
   content: z.array(z.record(z.string(), z.unknown())).optional()
 });
 
--- src/validators/userValidator.ts
+++ src/validators/userValidator.ts
@@ -1,28 +1,36 @@
 import { z } from 'zod';
+import { getRangeError } from '../lib';
+
+const passwordError = getRangeError('password', 8, 100);
 
 const userCreate = z.object({
-  email: z.string().email(),
-  name: z.string().min(3).max(100),
-  password: z.string().min(8).max(100)
+  email: z.string().email('Invalid email'),
+  name: z.string().min(3).max(100, 'Name must be greater than 3 and less than 100'),
+  password: z.string().min(8).max(100, passwordError)
 });
 
 const userPassSchema = z.object({
-  password: z.string().min(8).max(100),
-  oldpassword: z.string().min(8).max(100)
+  password: z.string().min(8).max(100, passwordError),
+  oldpassword: z
+    .string()
+    .min(8)
+    .max(100, getRangeError('oldpassword', 8, 100))
 });
 
 const userLogin = z.object({
   email: z.string().email(),
-  password: z.string().min(8).max(100)
+  password: z.string().min(8).max(100, passwordError)
 });
 
 const userSocialUpdate = z.object({
   type: z.enum(['twitter', 'instagram', 'facebook', 'website']),
-  value: z.string().url()
+  value: z.string().url('Invalid URL')
 });
 
 const userImageUpdate = z.object({
-  image: z.string()
+  image: z.string().min(4, {
+    message: 'Invalid Image URL'
+  })
 });
 
 export { userLogin, userCreate, userPassSchema, userSocialUpdate, userImageUpdate };
