bdace33a3e1af2fab6cfecb7accf0c7e5c09284a
ChampionBuffalo1 <vidish7nov@gmail.com>
2023-08-03 22:34:27 +0530

    Moved query helpers to a separate folder

---
+++ src/models/query/blogQueries.ts
@@ -0,0 +1,201 @@
+import Blog from '../Blog';
+import { BlogType } from '../../typings/model';
+import { Nullable } from '../../typings/reset';
+import { updateSchema } from '../../validators/blogValidator';
+
+const projection = '-passwordHash -email -__v';
+
+type createBlogType = {
+  title: string;
+  slug: string;
+  authorId: string;
+  category: string;
+  content: Record<string, unknown>[];
+  coverImage?: string;
+  description?: string;
+}
+
+function createBlog({
+  title,
+  slug,
+  authorId,
+  category,
+  content,
+  coverImage,
+  description
+}: createBlogType): Promise<BlogType> {
+  const blog = new Blog({
+    slug,
+    title,
+    content,
+    category,
+    coverImage,
+    description,
+    author: authorId,
+    isPublished: false,
+    lastEditedAt: new Date()
+  });
+  return blog.save();
+}
+
+function updateBlog(_id: string, data: updateSchema): Promise<Nullable<BlogType>> {
+  return Blog.findOneAndUpdate(
+    {
+      _id
+    },
+    {
+      $set: {
+        ...data,
+        isPublished: false,
+        lastEditedAt: new Date()
+      }
+    },
+    {
+      new: true
+    }
+  )
+    .populate('author', projection)
+    .exec();
+}
+
+function deleteBlog(slug: string, id?: string): Promise<Nullable<BlogType>> {
+  const query: Record<string, string> = { slug };
+  // if id isn't provided, and this fn is called that it means that the user was admin/manager
+  if (id) query['author'] = id;
+  return Blog.findOneAndDelete(query, {
+    new: true
+  })
+    .populate('author', projection)
+    .exec();
+}
+function publishBlog(slug: string): Promise<Nullable<BlogType>> {
+  return Blog.findOneAndUpdate(
+    {
+      slug
+    },
+    {
+      $set: {
+        isPublished: true
+      }
+    },
+    {
+      new: true
+    }
+  )
+    .populate('author', projection)
+    .exec();
+}
+
+function getAllBlogs(limit?: number, skip?: number): Promise<BlogType[]> {
+  const query = Blog.find(
+    {
+      isPublished: true
+    },
+    '-content -__v',
+    { lean: true }
+  ).populate('author', projection);
+  if (skip && skip > 0) query.skip(skip);
+  if (limit) query.limit(limit);
+  return query.exec();
+}
+
+function getUnpublishedBlogsOfUser(id: string): Promise<BlogType[]> {
+  return Blog.find(
+    {
+      author: id,
+      isPublished: false
+    },
+    '-content -__v',
+    {
+      lean: true
+    }
+  )
+    .populate('author', projection)
+    .exec();
+}
+
+function getBlogByCategory(category: string, limit?: number, skip?: number): Promise<BlogType[]> {
+  const query = Blog.find(
+    {
+      category,
+      isPublished: true
+    },
+    null
+  ).populate('author', projection);
+  if (skip && skip > 0) query.skip(skip);
+  if (limit) query.limit(limit);
+  return query.exec();
+}
+
+function getPopularBlogs(limit?: number, skip?: number): Promise<BlogType[]> {
+  const query = Blog.find(
+    {
+      isPublished: true
+    },
+    null,
+    {
+      sort: {
+        views: -1
+      }
+    }
+  ).populate('author', projection);
+  if (skip && skip > 0) query.skip(skip);
+  if (limit) query.limit(limit);
+  return query.exec();
+}
+
+async function getBlogBySlug(slug: string): Promise<Nullable<BlogType>> {
+  const blog = Blog.findOne(
+    {
+      slug
+    },
+    null,
+    {
+      lean: true
+    }
+  )
+    .populate('author', projection)
+    .exec();
+  await Blog.findOneAndUpdate(
+    {
+      slug
+    },
+    {
+      $inc: {
+        views: 1
+      }
+    }
+  );
+  return blog;
+}
+
+function getAllUniqueCategory(): Promise<string[]> {
+  return Blog.find({
+    isPublished: true
+  })
+    .distinct('category')
+    .exec();
+}
+
+function getUnpublishedBlogContent(author?: string, otherCond?: Record<string, unknown>) {
+  const query = Blog.findOne({
+    ...otherCond,
+    isPublished: false
+  }).populate('author', projection);
+  if (author) query.where('author').equals(author);
+  return query.exec();
+}
+
+export = {
+  createBlog,
+  deleteBlog,
+  updateBlog,
+  publishBlog,
+  getAllBlogs,
+  getBlogBySlug,
+  getPopularBlogs,
+  getBlogByCategory,
+  getAllUniqueCategory,
+  getUnpublishedBlogContent,
+  getUnpublishedBlogsOfUser
+};
---
+++ src/models/query/userQueries.ts
@@ -0,0 +1,112 @@
+import User from '../User';
+import { Nullable } from '../../typings/reset';
+import { UserType } from '../../typings/model';
+
+function createUser(name: string, email: string, passwordHash: string): Promise<UserType> {
+  const user = new User({
+    name,
+    email,
+    passwordHash
+  });
+  return user.save();
+}
+
+function editUser(id: string, updateDoc: Partial<Omit<UserType, 'id'>>): Promise<Nullable<UserType>> {
+  return User.findByIdAndUpdate(
+    {
+      _id: id
+    },
+    updateDoc,
+    {
+      lean: true
+    }
+  );
+}
+
+function deleteUser(id: string): Promise<Nullable<UserType>> {
+  return User.findOneAndDelete(
+    {
+      _id: id
+    },
+    {
+      lean: true
+    }
+  );
+}
+
+function getUserByEmail(email: string, projection?: string): Promise<Nullable<UserType>> {
+  return User.findOne(
+    {
+      email
+    },
+    projection
+  );
+}
+
+function getUser(id: string, projection?: string): Promise<Nullable<UserType>> {
+  return User.findById(
+    {
+      _id: id
+    },
+    projection,
+    {
+      lean: true
+    }
+  );
+}
+
+function addSocialMedia(
+  _id: string,
+  type: 'website' | 'twitter' | 'instagram' | 'facebook',
+  value: string
+): Promise<Nullable<UserType>> {
+  const setObj: Record<string, string> = {};
+  setObj[`socialMedia.${type}`] = value;
+  return User.findByIdAndUpdate(
+    {
+      _id
+    },
+    {
+      $set: setObj
+    },
+    {
+      projection: {
+        name: true,
+        socialMedia: true
+      },
+      new: true,
+      lean: true
+    }
+  );
+}
+
+function addImage(_id: string, image: string): Promise<Nullable<UserType>> {
+  return User.findByIdAndUpdate(
+    {
+      _id
+    },
+    {
+      $set: {
+        image
+      }
+    },
+    {
+      projection: {
+        name: true,
+        image: true
+      },
+      new: true,
+      lean: true
+    }
+  );
+}
+
+async function userExists(_id: string): Promise<boolean> {
+  return !!(
+    await User.exists({
+      _id
+    })
+  )?._id;
+}
+
+export = { createUser, editUser, deleteUser, getUser, userExists, getUserByEmail, addSocialMedia, addImage };
